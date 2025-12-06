a54f6c11aa06ad882e309652ea268a133b8eab08
KoalaWill <108674407+KoalaWill@users.noreply.github.com>
2025-12-07 00:57:34 +0800

    Add BFS check accessibility

--- src/main.c
+++ src/main.c
@@ -10,34 +10,30 @@
 
 // Maneuver array
 const int Mode_Movement_Fuel[4][8][4]={/*mode 0*/{{0,0,1,1},{0,0,-1,1},{0,1,0,3},{0,-1,0,3},{1,2,0,3},{1,1,-1,3},{3,0,-2,3},{3,-1,-1,3}},
-									   /*mode 1*/{{1,1,0,1},{1,-1,0,1},{1,0,-1,3},{1,0,1,3},{2,0,-2,3},{2,-1,-1,3},{0,-2,0,3},{0,-1,1,3}},
-									   /*mode 2*/{{2,0,-1,1},{2,0,1,1},{2,-1,0,3},{2,1,0,3},{3,-2,0,3},{3,-1,1,3},{1,0,2,3},{1,1,1,3}},
-									   /*mode 3*/{{3,-1,0,1},{3,1,0,1},{3,0,1,3},{3,0,-1,3},{0,0,2,3},{0,1,1,3},{2,2,0,3},{2,1,-1,3}}
+                                       /*mode 1*/{{1,1,0,1},{1,-1,0,1},{1,0,-1,3},{1,0,1,3},{2,0,-2,3},{2,-1,-1,3},{0,-2,0,3},{0,-1,1,3}},
+                                       /*mode 2*/{{2,0,-1,1},{2,0,1,1},{2,-1,0,3},{2,1,0,3},{3,-2,0,3},{3,-1,1,3},{1,0,2,3},{1,1,1,3}},
+                                       /*mode 3*/{{3,-1,0,1},{3,1,0,1},{3,0,1,3},{3,0,-1,3},{0,0,2,3},{0,1,1,3},{2,2,0,3},{2,1,-1,3}}
 }; // mode, dx, dy, fuel cost
-									   
-									   // MODE 0     MODE 1       MODE 2      MODE 3
-									   // X O        O O X        O O         O O O
-									   // O O        O O O        O O         X O O
-									   // O O                     O X
-
-                                       // App States
+                                       
+// App States
 typedef enum AppScreen { 
-	StartMenu = 0, 
-	MazeConfirm,
+    StartMenu = 0, 
+    MazeConfirm,
     AccessibilityCheck
 } AppScreen;
 
-// Start menu display variables
-// Window
+// Window variables
 const int screenWidth = 1280;
 const int screenHeight = 800;
 int currentX;
 int currentY;
+
 // Maze variables
 int **maze = NULL;
 int rows = 0;
 int cols = 0;
 bool mazeLoaded = false;
+
 // Maze display variables
 int mazeDisplayMargin;
 int availableWidth;
@@ -47,8 +43,8 @@ int mazePixelWidth;
 int mazePixelHeight;
 int offsetX;
 int offsetY;
+
 // Accessibility check variables
-// queue
 typedef struct {
     int x, y, mode;
 } State;
@@ -74,47 +70,43 @@ State dequeue(Queue* q) {
 }
 Queue* q;
 State start_state;
-// A simple list to remember where objectives are
 struct { int x, y; bool reachable; } objectives[100]; 
 int objCount = 0;
 bool visited[MAX_ROWS][MAX_COLS][4];
 int reachableCount = 0;
 int nextMode;
-int dx;
-int dy;
-int nx;
-int ny;
+int dxdy[2];
+int nx, ny;
+bool accessChecked;
 
 // Start menu
-// ASCII art
 const char *ascii_art[] = {
     " ________  ________  _________  ___  ___  ________ ___  ________   ________  _______   ________   ",
     "|\\   __  \\|\\   __  \\|\\___   ___\\\\  \\|\\  \\|\\  _____\\\\  \\|\\   ___  \\|\\   ___ \\|\\   ____\\|\\   __  \\  ",
     "\\ \\  \\|\\  \\ \\  \\|\\  \\|___ \\  \\_\\ \\  \\\\\\  \\ \\  \\___| \\  \\ \\  \\\\ \\  \\ \\  \\_|\\ \\ \\  \\___|\\ \\  \\|\\  \\ ",
-    " \\ \\   ____\\ \\   __  \\   \\ \\  \\ \\ \\   __  \\ \\   __\\\\ \\  \\ \\  \\\\ \\  \\ \\  \\ \\\\ \\ \\   ____\\ \\  __  _\\",
-    "  \\ \\  \\___|\\ \\  \\ \\  \\   \\ \\  \\ \\ \\  \\ \\  \\ \\  \\_| \\ \\  \\ \\  \\\\ \\  \\ \\  \\_\\\\ \\ \\  \\___|\\ \\ \\ \\ \\|",
+    " \\ \\   ____\\ \\   __  \\   \\ \\  \\ \\ \\   __  \\ \\   __\\\\ \\  \\ \\  \\\\ \\  \\ \\  \\ \\\\ \\ \\   ____\\ \\   __  _\\",
+    "  \\ \\  \\___|\\ \\  \\ \\  \\   \\ \\  \\ \\ \\  \\ \\  \\ \\  \\_| \\ \\  \\ \\  \\\\ \\  \\ \\  \\_\\\\ \\ \\  \\___|\\ \\  \\ \\|",
     "   \\ \\__\\    \\ \\__\\ \\__\\   \\ \\__\\ \\ \\__\\ \\__\\ \\__\\   \\ \\__\\ \\__\\\\ \\__\\ \\_______\\ \\_______\\ \\_\\ \\_\\",
     "    \\ |__|     \\|__|\\|__|    \\|__|  \\|__|\\|__|\\|__|    \\|__|\\|__| \\|__|\\|_______|\\|_______|\\|_|\\|_|"
 };
 void DrawGradientTitle() {
-    // Gradient blue to orange
     Color startColor = {0, 100, 255, 255};
     Color endColor = {255, 140, 0, 255};
-	// text var
+    
     int artRows = sizeof(ascii_art) / sizeof(ascii_art[0]);
     int fontSize = 16;
     int charSpacing = 12;
     int lineHeight = fontSize + 2;
-    // Center title
+    
     int TitletotalWidth = strlen(ascii_art[0]) * charSpacing;
     int TitletotalHeight = artRows * lineHeight;
     int startX = (screenWidth - TitletotalWidth) / 2;
-    int startY = (screenHeight - TitletotalHeight) / 2 - 80; // nudge up little
-    // Draw title
+    int startY = (screenHeight - TitletotalHeight) / 2 - 80;
+
     for (int i = 0; i < artRows; i++) {
-		int len = strlen(ascii_art[i]);
+        int len = strlen(ascii_art[i]);
         for (int j = 0; j < len; j++) {
-			float t = (float)j / len;
+            float t = (float)j / len;
             unsigned char r = (unsigned char)(startColor.r + t * (endColor.r - startColor.r));
             unsigned char g = (unsigned char)(startColor.g + t * (endColor.g - startColor.g));
             unsigned char b = (unsigned char)(startColor.b + t * (endColor.b - startColor.b));
@@ -123,40 +115,37 @@ void DrawGradientTitle() {
             DrawText(tempStr, startX + (j * charSpacing), startY + (i * lineHeight), fontSize, drawColor);
         }
     }
-	// Move cursor
-	currentY = startY + TitletotalHeight + 30;
+    currentY = startY + TitletotalHeight + 30;
 }
 void DrawCredits() {
-	const char* sub1 = "Programming Final Project | Time: December 2025 | Author: William Lin | ID: 114033213 | Dept: PME";
-	const char* sub2 = "Using Raylib";
-	
-	int subSize = 20; 
-	int subWidth1 = MeasureText(sub1, subSize);
-	int subWidth2 = MeasureText(sub2, subSize);
-	
+    const char* sub1 = "Programming Final Project | Time: December 2025 | Author: William Lin | ID: 114033213 | Dept: PME";
+    const char* sub2 = "Using Raylib";
+    
+    int subSize = 20; 
+    int subWidth1 = MeasureText(sub1, subSize);
+    int subWidth2 = MeasureText(sub2, subSize);
+    
     DrawText(sub1, (screenWidth - subWidth1)/2, currentY, subSize, WHITE);
     currentY += 25;
     DrawText(sub2, (screenWidth - subWidth2)/2, currentY, subSize, WHITE);
 }
 void DrawBlinkHint() {
-    // Draw Instruction Text below (Blinking)
     const char* msg = "PRESS [ENTER] TO VIEW MAZE";
     int textWidth = MeasureText(msg, 20);
     if (((int)(GetTime() * 2)) % 2 == 0) {
-        // Positioned further down below the subtitles
         DrawText(msg, (screenWidth - textWidth)/2, currentY + 50, 20, WHITE);
     }
-    
 }
 
-// Maze page 
+// Maze confirmation
 bool LoadMaze(const char *filename) {
-	rows = 0; cols = 0;
+    rows = 0; cols = 0;
     bool col_calculated = false;
     int ch;
-	
+    
     FILE *inf = fopen(filename, "r");
     if (inf == NULL) return false;
+    
     // Calculate rows and cols
     while (1) {
         ch = fgetc(inf);
@@ -171,13 +160,12 @@ bool LoadMaze(const char *filename) {
             rows++;
         }
     }
-	printf("\n");
-    // Make memory space
+    
     maze = (int **)malloc(rows * sizeof(int *));
     for (int i = 0; i < rows; i++) {
         maze[i] = (int *)malloc(cols * sizeof(int));
     }
-    // rewind and read in data
+    
     rewind(inf);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -187,32 +175,32 @@ bool LoadMaze(const char *filename) {
             if(maze[i][j] == 2){
                 start_state.x = j;
                 start_state.y = i;
-                if(maze[i][j-1] == 2 && maze[i][j-2] == 2) {
-                    start_state.mode = 3;
-                    start_state.x = j-2;
+                start_state.mode = 0; // Default mode 0
+                // Heuristic check for mode based on vehicle size/orientation
+                if (j >= 2 && maze[i][j-1] == 2 && maze[i][j-2] == 2) {
+                     start_state.mode = 3;
+                     start_state.x = j-2;
                 }
-                if(maze[i][j-1] == 2) {
-                    start_state.mode = 2;
+                else if (j >= 1 && maze[i][j-1] == 2) {
+                     start_state.mode = 2; // Assuming
                 }
             }
         }
     }
-	fclose(inf);
+    fclose(inf);
     return true;
 }
 void DrawMazeGrid() {
-	// Check maze
     if (!mazeLoaded || rows == 0 || cols == 0) {
         DrawText("Error: Maze not loaded or empty.", 100, 100, 20, ORANGE);
         return;
     }
 
-	// Size calculations
     mazeDisplayMargin = 40;
     availableWidth = screenWidth - (mazeDisplayMargin*2);
     availableHeight = screenHeight - (mazeDisplayMargin*2);
     cellSize = (availableWidth/cols < availableHeight/rows)? availableWidth/cols : availableHeight/rows; 
-	mazePixelWidth = cols * cellSize;
+    mazePixelWidth = cols * cellSize;
     mazePixelHeight = rows * cellSize;
     offsetX = (screenWidth-mazePixelWidth) / 2;
     offsetY = (screenHeight-mazePixelHeight) / 2;
@@ -227,21 +215,20 @@ void DrawMazeGrid() {
             switch(val) {
                 case 0: c = DARKGRAY; break;// Wall
                 case 1: c = LIME; break;// Accessible
-				case 2: c = MAROON; break;// Starting point
-				case 3: c = BLUE; break;// Objective
-				default: c = BLACK;
+                case 2: c = MAROON; break;// Starting point
+                case 3: c = BLUE; break;// Objective
+                default: c = BLACK;
             }
 
             DrawRectangle(x, y, cellSize, cellSize, c);
-            
-            // border
             DrawRectangleLines(x, y, cellSize, cellSize, BLACK);
         }
     }
     DrawText(TextFormat("Maze input detected, maze size: %d(rows) x %d(columns)", rows, cols), 10, screenHeight - 30, 20, GRAY);
     DrawText("PRESS [ENTER] TO CONFIRM MAZE MAP", 10, 10, 20, LIGHTGRAY);
 }
-// collision checker
+
+// Accessibility check
 int CheckCarCollision(int x, int y, int mode) {
     int body[6][2]; 
 
@@ -261,11 +248,14 @@ int CheckCarCollision(int x, int y, int mode) {
     }
     return 1; // safe
 }
-
-// accessibility check via BFS
 void CheckAccessibility() {
-    Queue* q = createQueue(MAX_ROWS * MAX_COLS * 4);
+    q = createQueue(MAX_ROWS * MAX_COLS * 4);
+    objCount=0;
     memset(visited, 0, sizeof(visited));
+    
+    // Reset Reachable Count
+    reachableCount = 0;
+
     for(int r=0; r<rows; r++){
         for(int c=0; c<cols; c++){
             if(maze[r][c] == 3) {
@@ -276,27 +266,31 @@ void CheckAccessibility() {
             }
         }
     }
+    
     if (CheckCarCollision(start_state.x, start_state.y, start_state.mode)) {
         visited[start_state.y][start_state.x][0] = true;
-        enqueue(q, (State){start_state.x, start_state.y, 0});
-    } else {
-        printf("Error: Car cannot fit at start position in Mode%d.\n", start_state.mode);
-        return;
+        enqueue(q, (State){start_state.x, start_state.y, start_state.mode});
     }
-
+    
     // BFS
     while (!isQueueEmpty(q)) {
         State current = dequeue(q);
+        
+        // Check if we hit an objective (this check is simplistic, usually check vehicle body overlap)
+        // For grid based, we assume if the reference point or nearby is the objective. 
+        // Based on original code logic:
         if (maze[current.y][current.x] == 3) {
-            // mark objective reachable
-            for(int i=0; i<objCount; i++) {
+             for(int i=0; i<objCount; i++) {
                 if(objectives[i].x == current.x && objectives[i].y == current.y) {
-                    objectives[i].reachable = true;
+                    if(!objectives[i].reachable) {
+                        objectives[i].reachable = true;
+                    }
                 }
             }
         }
 
         // try all directions
+        int dx, dy;
         for (int i = 0; i < 8; i++) {
             nextMode = Mode_Movement_Fuel[current.mode][i][0];
             dx = Mode_Movement_Fuel[current.mode][i][1];
@@ -316,64 +310,111 @@ void CheckAccessibility() {
             }
         }
     }
-    // print it out.
-    printf("Reachable analysis via BFS:\n");
-    objCount = 0;
-    for(int i=0; i<objCount; i++) {
-        if(objectives[i].reachable) {
-            printf("Objective at (%d, %d) is ACCESSIBLE.\n", objectives[i].x, objectives[i].y);
-            reachableCount++;
-        } else {
-            printf("Objective at (%d, %d) is UNREACHABLE.\n", objectives[i].x, objectives[i].y);
-        }
-    }
     
-    printf("\nSummary: %d / %d objectives reachable.\n", reachableCount, objCount);
+    // Final tally
+    for(int i=0; i<objCount; i++) {
+        if(objectives[i].reachable) reachableCount++;
+    }
 
     // Cleanup
     free(q->items);
     free(q);
-    free(visited);
+}
+void DrawAccessibilityResults() {
+    int startY = 100;
+    int spacing = 30;
+    int titleSize = 40;
+    int textSize = 24;
+
+    const char* title = "ACCESSIBILITY ANALYSIS RESULTS";
+    int titleWidth = MeasureText(title, titleSize);
+    DrawText(title, (screenWidth - titleWidth)/2, 40, titleSize, SKYBLUE);
+
+    DrawLine(100, 85, screenWidth-100, 85, DARKGRAY);
+
+    for(int i=0; i<objCount; i++) {
+        // Build the result string
+        const char* status = objectives[i].reachable ? "ACCESSIBLE" : "UNREACHABLE";
+        Color statusColor = objectives[i].reachable ? LIME : RED;
+        
+        char buffer[100];
+        sprintf(buffer, "Objective at (%d, %d): ", objectives[i].x, objectives[i].y);
+        
+        // Draw coordinate text
+        int textW = MeasureText(buffer, textSize);
+        DrawText(buffer, 150, startY + (i*spacing), textSize, LIGHTGRAY);
+        
+        // Draw status text next to it
+        DrawText(status, 150 + textW + 10, startY + (i*spacing), textSize, statusColor);
+    }
+
+    // Draw Summary
+    int summaryY = startY + (objCount * spacing) + 40;
+    DrawLine(100, summaryY - 20, screenWidth-100, summaryY - 20, DARKGRAY);
+    
+    char summary[100];
+    sprintf(summary, "SUMMARY: %d / %d objectives reachable.", reachableCount, objCount);
+    DrawText(summary, 150, summaryY, 30, WHITE);
+
+    // Bottom prompt
+    const char* prompt = "PRESS [ENTER] TO EXIT";
+    if (((int)(GetTime() * 2)) % 2 == 0) {
+        DrawText(prompt, (screenWidth - MeasureText(prompt, 20))/2, screenHeight - 50, 20, GRAY);
+    }
 }
 
 int main(void) {
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
     InitWindow(screenWidth, screenHeight, "Pathfinder GUI");
-	mazeLoaded = false;
-	// read input
+    
+    mazeLoaded = false;
+    accessChecked = false;
+    
+    // read input
     if(!mazeLoaded) mazeLoaded = LoadMaze("input.txt");
-	// init state
+    
+    // init state
     AppScreen currentScreen = StartMenu;
 
     while (!WindowShouldClose()) {
-		// State control
+        // State control logic
         if (currentScreen == StartMenu) {
             if (IsKeyPressed(KEY_ENTER) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                 currentScreen = MazeConfirm;
             }
         }
-		else if (currentScreen == MazeConfirm) {
-			if (IsKeyPressed(KEY_ENTER) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-				currentScreen = AccessibilityCheck;
-			}
-		}
+        else if (currentScreen == MazeConfirm) {
+            if (IsKeyPressed(KEY_ENTER) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                currentScreen = AccessibilityCheck;
+            }
+        }
+        else if (currentScreen == AccessibilityCheck) {
+            if(!accessChecked) {
+                CheckAccessibility();
+                accessChecked = true;
+            }
+            if (IsKeyPressed(KEY_ENTER)) {
+                break;
+            }
+        }
 
         BeginDrawing();
             ClearBackground(BLACK);
+            
             switch(currentScreen) {
                 case StartMenu:
-                    ClearBackground(BLACK);
                     DrawGradientTitle();
-					DrawCredits();
-					DrawBlinkHint();
+                    DrawCredits();
+                    DrawBlinkHint();
                     break;
+                    
                 case MazeConfirm:
-                    ClearBackground(BLACK);
                     DrawMazeGrid();
                     break;
+                    
                 case AccessibilityCheck:
-                    ClearBackground(BLACK);
-                    CheckAccessibility();
+                    DrawAccessibilityResults();
+                    break;
             }
 
         EndDrawing();
